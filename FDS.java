78157891c2b9b677e833f9359fae00d7e6585a2f
Aarya Patankar <patankaraarya803@gmail.com>
2025-04-16 16:46:06 +0530

    Update FDS.java

--- FDS.java
+++ FDS.java
@@ -4,14 +4,14 @@ public class FDS {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Graph graph = new Graph();
-	System.out.print("Enter fraud threshold (default is ₹10000): ");
+	System.out.print("Enter fraud threshold (default is Rs.10000): ");
 	String input = scanner.nextLine();
 	if (!input.isEmpty()) {
     		try {
         		double threshold = Double.parseDouble(input);
         		graph.setFraudThreshold(threshold);
     		} catch (NumberFormatException e) {
-        		System.out.println("⚠ Invalid input. Using default ₹10000.");
+        		System.out.println("Invalid input. Using default Rs.10000.");
     		}
 	}
 
@@ -24,7 +24,8 @@ public class FDS {
 	    System.out.println("5. Load Sample Fraud Dataset");
 	    System.out.println("6. Set Fraud Threshold");
 	    System.out.println("7. Display Graph Connections");
-            System.out.println("8. Exit");
+	    System.out.println("8. Run BFS Fraud Detection (All Users)");
+            System.out.println("9. Exit");
             System.out.print("Enter your choice: ");
 
             int choice = scanner.nextInt();
@@ -60,7 +61,7 @@ public class FDS {
    		    loadSampleData(graph);
  		    break;
 		case 6:
-    		    System.out.print("Enter new fraud threshold (₹): ");
+    		    System.out.print("Enter new fraud threshold (Rs.): ");
     		    double newThreshold = scanner.nextDouble();
     		    scanner.nextLine();
    		    graph.setFraudThreshold(newThreshold);
@@ -68,7 +69,10 @@ public class FDS {
 		case 7:
    		    graph.displayGraphAsEdges();
                     break;
-                case 8:
+		case 8:
+    		    graph.detectFraudBFSAll();
+    		    break;
+                case 9:
                     System.out.println("Exiting...");
                     scanner.close();
                     return;
@@ -77,16 +81,15 @@ public class FDS {
             }
         }
     }
-public static void loadSampleData(Graph graph) {
-    graph.addTransaction("A", "B", 2000, "2025-04-01 10:00:00");
-    graph.addTransaction("B", "C", 3000, "2025-04-01 10:01:00");
-    graph.addTransaction("C", "D", 4000, "2025-04-01 10:02:00");
-    graph.addTransaction("D", "A", 5000, "2025-04-01 10:03:00"); // cycle
+	public static void loadSampleData(Graph graph) {
+   		graph.addTransaction("A", "B", 2000, "2025-04-01 10:00:00");
+    		graph.addTransaction("B", "C", 3000, "2025-04-01 10:01:00");
+    		graph.addTransaction("C", "D", 4000, "2025-04-01 10:02:00");
+    		graph.addTransaction("D", "A", 5000, "2025-04-01 10:03:00"); // cycle
+    		graph.addTransaction("X", "Y", 15000, "2025-04-01 11:00:00"); // high-value
+    		graph.addTransaction("P", "Q", 700, "2025-04-01 12:00:00");   // normal
 
-    graph.addTransaction("X", "Y", 15000, "2025-04-01 11:00:00"); // high-value
-    graph.addTransaction("P", "Q", 700, "2025-04-01 12:00:00");   // normal
-
-    System.out.println("Sample dataset loaded.");
-}
+    		System.out.println("Sample dataset loaded.");
+	}
 
 } 
